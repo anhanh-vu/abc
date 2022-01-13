fb918d0cb8109e367a86c43583441ce25a4313f8
anhanh-vu <85780716+anhanh-vu@users.noreply.github.com>
2022-01-14 01:03:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hello, người lạ!',
+    introTitle: 'Hello, người quen!',
     introDesc: `Mình muốn bảo cậu là *(^O^)*!`,
-    btnIntro: 'Ừa',
+    btnIntro: 'Ừa',�
     title: 'Cho kẹo hay bị ghẹo. <33333 😙',
-    desc: ' Cho thì cho mà không cho cũng phải choooooo ! <33333',
+    desc: ' Cho thì cho mà không cho cũng phải choooooo ! 😋😌',
     btnYes: 'okeeeee Đồng ý luôn ! <3 ',
     btnNo: 'Không cho thì sao!!!',
-    question:'Tết nhớ lì xì :-P!!!',
-    btnReply: 'Okeeee <3',
-    reply: 'Noooooo <33333333',
+    question:'Tết nhớ lì xì 😜!!!',
+    btnReply: 'Okeeee 👌👌👌',
+    reply: 'Nooo',
     mess: 'Bạn đã bị lừa :v',
-    messDesc: 'Hmmmm <3 .',
-    btnAccept: 'inbox để lì xì  <3',
+    messDesc: 'Hmmmm🤭🤭🤭 <3 .',
+    btnAccept: 'inbox để lì xì mình😆 ',
     messLink: 'https://www.facebook.com/vuanh.psyduck'
 }
