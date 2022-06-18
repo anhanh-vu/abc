f7a60ef44318d7227525e897f820c8891060b9a8
anhanh-vu <ngocanhvu2508@gmail.com>
2022-06-18 17:49:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,26 @@
 const CONFIG = {
-    introTitle: 'Hello, người quen!',
+    introTitle: 'Hello, người lạ!',
     introDesc: `Mình muốn bảo cậu là *(^O^)*!`,
+<<<<<<< HEAD
     btnIntro: 'Ừa 🙄',
+=======
+    btnIntro: 'Ừa',
+>>>>>>> parent of fb918d0 (Update config.js)
     title: 'Cho kẹo hay bị ghẹo. <33333 😙',
-    desc: ' Cho thì cho mà không cho cũng phải choooooo ! 😋😌',
+    desc: ' Cho thì cho mà không cho cũng phải choooooo ! <33333',
     btnYes: 'okeeeee Đồng ý luôn ! <3 ',
     btnNo: 'Không cho thì sao!!!',
-    question:'Tết nhớ lì xì 😜!!!',
-    btnReply: 'Okeeee 👌👌👌',
-    reply: 'Nooo',
+    question:'Tết nhớ lì xì :-P!!!',
+    btnReply: 'Okeeee <3',
+    reply: 'Noooooo <33333333',
     mess: 'Bạn đã bị lừa :v',
+<<<<<<< HEAD
     messDesc: 'Hmmmm🤭🤭🤭 <3 .',
     btnAccept: 'inbox để lì xì mình😆 ',
     messLink: 'https://www.facebook.com/vuuaannh'
+=======
+    messDesc: 'Hmmmm <3 .',
+    btnAccept: 'inbox để lì xì  <3',
+    messLink: 'https://www.facebook.com/vuanh.psyduck'
+>>>>>>> parent of fb918d0 (Update config.js)
 }
