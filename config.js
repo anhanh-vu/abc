83572b15c82045f41d81afb7492e079db96558c5
anhanh-vu <ngocanhvu2508@gmail.com>
2022-06-18 18:03:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 const CONFIG = {
     introTitle: 'Hello, người lạ!',
     introDesc: `Mình muốn bảo cậu là *(^O^)*!`,
-<<<<<<< HEAD
     btnIntro: 'Ừa 🙄',
-=======
     btnIntro: 'Ừa',
->>>>>>> parent of fb918d0 (Update config.js)
     title: 'Cho kẹo hay bị ghẹo. <33333 😙',
     desc: ' Cho thì cho mà không cho cũng phải choooooo ! <33333',
     btnYes: 'okeeeee Đồng ý luôn ! <3 ',
@@ -14,13 +11,10 @@ const CONFIG = {
     btnReply: 'Okeeee <3',
     reply: 'okeeee lunnnn <33333333',
     mess: 'Bạn đã bị lừa :v',
-<<<<<<< HEAD
     messDesc: 'Hmmmm🤭🤭🤭 <3 .',
     btnAccept: 'inbox để cho kẹo mình😆 ',
     messLink: 'https://www.facebook.com/vuuaannh'
-=======
     messDesc: 'Hmmmm <3 .',
     btnAccept: 'inbox để lì xì  <3',
     messLink: 'https://www.facebook.com/vuanh.psyduck'
->>>>>>> parent of fb918d0 (Update config.js)
 }
